155858c54d80e140dcf00e7f8dc9c27b9a9b8330
Ivor Scott <ivor@devpie.io>
2021-04-11 19:30:44 +0200

    fix: image

--- kubernetes-config/certs.tf
+++ kubernetes-config/certs.tf
@@ -1,5 +1,10 @@
 resource "aws_acm_certificate" "cert" {
   domain_name       = var.hostname
+  
+  subject_alternative_names = [
+    "*.${var.hostname}",
+  ]
+
   validation_method = "DNS"
 
   tags = {
