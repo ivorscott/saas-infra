1723ac66065cc5ebf2575dc0322feb958a576bcf
Ivor Scott <ivor@devpie.io>
2021-04-11 21:46:44 +0200

    fix: setup ingress script

--- setup_ingress.sh
+++ setup_ingress.sh
@@ -12,20 +12,20 @@ metadata:
     alb.ingress.kubernetes.io/actions.ssl-redirect: '{"Type": "redirect", "RedirectConfig": { "Protocol": "HTTPS", "Port": "443", "StatusCode": "HTTP_301"}}'
     alb.ingress.kubernetes.io/backend-protocol: HTTP
     alb.ingress.kubernetes.io/certificate-arn: $1
+    external-dns.alpha.kubernetes.io/hostname: $2
   labels:
     app: traefik
 spec:
-  rules:
-    - host: $2
-      http:
-        paths:
-          # HTTP to HTTPS redirect entry
-          - path: /*
-            backend:
-              serviceName: ssl-redirect
-              servicePort: use-annotation
-          - path: /*
-            backend:
-              serviceName: traefik
-              servicePort: 443
+  - rules:
+    http:
+      paths:
+        # HTTP to HTTPS redirect entry
+        - path: /*
+          backend:
+            serviceName: ssl-redirect
+            servicePort: use-annotation
+        - path: /*
+          backend:
+            serviceName: traefik
+            servicePort: 443
 EOF
