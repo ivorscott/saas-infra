bc78ad1a9ec288d87054be8b20b07eb4b6317c2f
Ivor Scott <ivor@devpie.io>
2022-10-24 22:26:37 +0200

    testing argocd route #1

--- scripts/ports.sh
+++ scripts/ports.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 # This script port forwards cluster applications.
 
-# ArgoCD
-lsof -t -i:8080 | xargs -r kill
-kubectl port-forward service/argo-cd-argocd-server -n argocd 8080:443 > /dev/null 2>&1 &
-
 # NATS
 lsof -t -i:4222 | xargs -r kill
 kubectl port-forward service/nats-svc 4222:4222 > /dev/null 2>&1 &
